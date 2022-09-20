b897c19ff1f5a3c2502b415feab5d6b893b9ab50
Sahil Arora <arora.12@iitj.ac.in>
2022-09-20 19:49:20 +0530

    added code and screenshots for assignment

---
+++ code-Q1.py
@@ -0,0 +1,86 @@
+import matplotlib .pyplot as plt
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+from beautifultable import BeautifulTable
+
+digits = datasets.load_digits()
+
+train_frac = 0.8
+test_frac = 0.1
+dev_frac = 0.1
+
+n_samples = len(digits.images)
+data = digits.images.reshape(n_samples, -1)
+
+x_train, x_dev_test, y_train, y_dev_test = train_test_split(data, digits.target, test_size=1-train_frac, shuffle=True)
+
+
+x_test, x_dev, y_test, y_dev = train_test_split(data, digits.target, test_size=(dev_frac/(test_frac + dev_frac)), shuffle=True)
+
+GAMMA = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+C = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
+table_1 = BeautifulTable()
+table_1.column_headers = ["", "Train","Test","Dev"]
+
+h_param_comb = [{'gamma':g,'C':c} for g in GAMMA for c in C]
+assert len(h_param_comb) == len(GAMMA)*len(C)
+
+best_acc = 0.0
+best_param = None
+best_model = None
+
+for curr in h_param_comb:
+  hyper_params = curr
+  clf = svm.SVC()
+  clf.set_params(**hyper_params)
+  
+  clf.fit(x_train,y_train)
+
+  #Get dev set prediction
+  predicted_dev = clf.predict(x_dev)
+  #Dev set accuracy
+  acc = metrics.accuracy_score(y_pred=predicted_dev, y_true = y_dev)
+
+  #Finding the best accuracy
+  if(acc > best_acc):
+    best_acc = acc
+    best_model = clf
+    best_param = curr
+
+  #Test set prediction and accuracy
+  predicted_test = clf.predict(x_test)
+  acc_test = metrics.accuracy_score(y_pred=predicted_test, y_true = y_test)
+
+  #Train set prediction and accuracy
+  predicted_train = clf.predict(x_train)
+  acc_train = metrics.accuracy_score(y_pred=predicted_train, y_true = y_train)
+    
+  table_1.append_row([curr,  acc_train,  acc_test,  best_acc])
+
+
+print(table_1)
+
+#Get test set prediction
+predicted_test = best_model.predict(x_test)
+acc_test = metrics.accuracy_score(y_pred=predicted_test, y_true = y_test)
+# print(
+#     f"Classification metric: {best_model}:\n"
+#     f"{metrics.classification_report(y_test,predicted_test)}\n"
+# )
+
+predicted_train = best_model.predict(x_train)
+acc_train = metrics.accuracy_score(y_pred=predicted_train, y_true = y_train)
+# print(
+#     f"Classification metric: {best_model}:\n"
+#     f"{metrics.classification_report(y_train,predicted_train)}\n"
+# )
+
+#Best Hyperparamater and accuracy
+
+table = BeautifulTable()
+table.column_headers = ["", "Train","Test","Dev"]
+table.append_row([best_param,  acc_train,  acc_test,  best_acc])
+print(table)
+
+
+
