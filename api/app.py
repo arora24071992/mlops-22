edcb4f60d0187ab1a3eae3ff27b1efa82890fe0b
arora24071992 <sahilarora@Sahils-MacBook-Pro.local>
2022-11-14 09:48:03 +0530

    Take home quiz changes

--- api/app.py
+++ api/app.py
@@ -3,31 +3,42 @@ from flask import request
 from joblib import load
 
 app = Flask(__name__)
-model_path = "svm_gamma=0.0005_C=2.joblib"
-model = load(model_path)
 
 @app.route("/")
 def hello_world():
     return "<!-- hello --> <b> Hello, World!</b>"
 
-
-# get x and y somehow    
-#     - query parameter
-#     - get call / methods
-#     - post call / methods ** 
-
-@app.route("/sum", methods=['POST'])
-def sum():
-    x = request.json['x']
-    y = request.json['y']
-    z = x + y 
-    return {'sum':z}
+#@app.route("/sum", methods=['POST'])
+#def sum():
+#    x = request.json['x']
+#    y = request.json['y']
+#    z = x + y 
+#    return {'sum':z}
 
 
+#Load one of the saved models
+model_path = "svm_Gamma=0.01_C=0.5.joblib"
+model = load(model_path)
+print("Model Loaded Successfully!")
 
+	
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-    image = request.json['image']
-    print("done loading")
-    predicted = model.predict([image])
-    return {"y_predicted":int(predicted[0])}
+
+	image1 = request.json['image1']
+	image2 = request.json['image2']
+	
+	predicted = model.predict([image1])
+	predicted_output_1 = int(predicted[0])
+	
+	predicted = model.predict([image2])
+	predicted_output_2 = int(predicted[0])
+
+	if predicted_output_1 == predicted_output_2:
+		return '\nImages are Same \n\n'
+	else:
+		return '\nImages are not Same \n\n'
+
+		
+if __name__ == "__main__":
+    app.run(debug=True)
